71a9077f2c8ab810dfa8f42fe3fa74f388cec73b
jadhavrg <rushijadhavrj9566@gmail.com>
2024-01-15 23:08:24 +0530

    Commit-4

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -8,27 +8,27 @@ function Skills() {
   return (
    <section id='skills'>
         <span className='skillsTitle'>What I do</span>
-        <span className='skillsDesc'>I have learned Techanology are HTML, CSS, SQL, Java, Rest API, JSON, J2EE, Hibernate, SpringBoot. </span>
+        <span className='skillsDesc'>I have learned Technology are HTML, CSS, SQL, Java, Rest API, JSON, J2EE, Hibernate, SpringBoot. </span>
         <div className='skillsBars'>
             <div className='skillsBar'>
                 <img src={UIDesign} alt='UIDesign' className='skillsBarImg'/>
                 <div className='skillsBarText'>
                     <h2>Front End Applications</h2>
-                    <p>I can build Front End Applications, Using Techanology like HTML, CSS, JS.</p>
+                    <p>I have build Front End Applications, Using Technology like HTML, CSS, JavaScript.</p>
                 </div>
             </div>
             <div className='skillsBar'>
                 <img src={AppDesign} alt='AppDesign' className='skillsBarImg'/>
                 <div className='skillsBarText'>
                     <h2>Back End Applications</h2>
-                    <p>I can also build Back End Applications, Using Techanology like Java, J2EE, Hibernate, SpringBoot.</p>
+                    <p>I have also build Back End Applications, Using Technology like Java, J2EE, Hibernate, SpringBoot.</p>
                 </div>
             </div>
             <div className='skillsBar'>
                 <img src={WebDesign} alt='WebDesign' className='skillsBarImg'/>
                 <div className='skillsBarText'>
                     <h2>Full stack Applications</h2>
-                    <p>So now I can also build Full Stack Applications, Using Techanology like HTML, CSS, JS, Java, J2EE, Hibernate, SpringBoot.  </p>
+                    <p>So now I have also build Full Stack Applications, Using Technology like HTML, CSS, JavaScript, Java, J2EE, Hibernate, SpringBoot.  </p>
                 </div>
             </div>
            
--- src/Components/Works/Works.js
+++ src/Components/Works/Works.js
@@ -18,15 +18,15 @@ function Works() {
             <div className='worksImg' >
               <div> <img src= {Bank} alt='ProjectImage' className='img-Style'></img> </div>
               <p><span>Name: </span>Bank Management System.</p>
-              <p><span>Techanologies: </span> HTML, CSS, Java, JDBC,<br/> Servlets, Jsp.</p>
-              <button className='worksBtn'><a href='' >Read More</a></button>
+              <p><span>Technologies: </span> HTML, CSS, Java, JDBC,<br/> Servlets, Jsp.</p>
+              <button className='worksBtn'><a href='https://github.com/jadhavrg/National_Banking_System' >Read More</a></button>
             </div>
 
             <div className='worksImg' >
               <div> <img src= {Hospital} alt='ProjectImage' className='img-Style'></img> </div>
               <p><span>Name: </span>Hospital Management System.</p>
-              <p><span>Techanologies: </span> Java, Hibernate, JSON, <br/> SpringBoot, Lambok.</p>
-              <button className='worksBtn'><a href='' >Read More</a></button>
+              <p><span>Technologies: </span> Java, Hibernate, JSON, <br/> SpringBoot, Lambok.</p>
+              <button className='worksBtn'><a href='https://github.com/jadhavrg/Hospital_Management_System' >Read More</a></button>
             </div>
 
             {/* <div className='worksImg' >
